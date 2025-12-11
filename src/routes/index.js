eef5d0b50b9ef6ee8275d0fe9c2f5bbe7698a129
Kazz02 <jesse.partonen@edu.turkuamk.fi>
2025-12-11 19:57:20 +0200

    Synced the database to index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,18 @@
 // src/routes/index.js
 const express = require("express");
 const healthRouter = require("./health.routes");
-
+const db = require("./models");
 const router = express.Router();
 
 router.use("/health", healthRouter);
 
+db.sequelize.sync({ force: false, alter: true})
+    .then(() => {
+        console.log("Database synchronized");
+    })
+    .catch((err) => {
+        console.error("Error synchronizing database:", err);
+    });
 // later
 // router.use("/auth", require("./auth.routes"));
 // router.use("/stores", require("./stores.routes"));
